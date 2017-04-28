43af965a9433cfc111cd8604652c9e260dd021ed
Mateo Mustapić <mateo.mustapic@liferay.com>
2017-04-28 10:00:40 +0200

    MATEO-3 - initialized first module

---
+++ mateo-mustapic/assignments/liferay-app/sdk/portlets/todo-portlet/docroot/js/main.js
@@ -0,0 +1,28 @@
+AUI.add(
+	'addCloseButton',
+	function (A) {
+		var addCloseButton = A.Component.create(
+			{
+				AUGMENTS: [Liferay.PortletBase],
+
+				EXTENDS: A.Base,
+
+				NAME: 'addClose',
+
+				prototype: {
+					initializer: function (config) {
+						var instance = this;
+
+						console.log('test');
+					}
+				}
+			}
+		);
+
+		Liferay.Portlet.addCloseButton = addCloseButton;
+	},
+	'',
+	{
+		requires: ['aui-event', 'aui-node', 'liferay-portlet-base', 'aui-modal']
+	}
+);
