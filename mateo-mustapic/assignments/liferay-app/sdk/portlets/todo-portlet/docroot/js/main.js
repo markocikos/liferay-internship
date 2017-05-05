b0333b2c968d1bd80f2f3fb8778da0cc618dbf36
Mateo Mustapić <mateo.mustapic@liferay.com>
2017-05-05 16:13:54 +0200

    MATEO-3 - initialized localization (for croatian language) - improved styling - input validation

--- mateo-mustapic/assignments/liferay-app/sdk/portlets/todo-portlet/docroot/js/main.js
+++ mateo-mustapic/assignments/liferay-app/sdk/portlets/todo-portlet/docroot/js/main.js
@@ -59,7 +59,7 @@ AUI.add(
 						var taskInput = instance._taskInput;
 						var todoList = instance._todoList;
 
-						if (taskInput && todoList) {
+						if (taskInput.val() !== '' && todoList) {
 							var taskHtml = A.Lang.sub(
 								TPL_TASK,
 								{
@@ -70,6 +70,10 @@ AUI.add(
 							todoList.append(taskHtml);
 
 							taskInput.val('');
+						}
+
+						else if (taskInput.val() === '') {
+							alert('Please enter some value');
 						}
 					}
 				}
