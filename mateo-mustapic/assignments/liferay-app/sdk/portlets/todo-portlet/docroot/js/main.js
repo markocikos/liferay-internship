13de9795af655adb412725c4138c9dc7bf8a8a1f
Mateo Mustapić <mateo.mustapic@liferay.com>
2017-05-24 14:30:42 +0200

    MATEO-3 - finished task template with time of appending task - appending and removing fisnihed tasks - character count - remaining tasks coun

--- mateo-mustapic/assignments/liferay-app/sdk/portlets/todo-portlet/docroot/js/main.js
+++ mateo-mustapic/assignments/liferay-app/sdk/portlets/todo-portlet/docroot/js/main.js
@@ -3,9 +3,19 @@ AUI.add(
 	function(A) {
 		var EVENT_CLICK = 'click';
 
+		var TPL_FINISHED = '<li>' +
+				'{taskFinished}' +
+				'<button>' +
+				'<i class="icon-trash"></i>' +
+				'</button>' +
+			'</li>';
+
 		var TPL_TASK = '<li>' +
 				'{task}' +
+				' ' +
+				'<button class="btn delete-task">' +
 				'<i class="icon-remove"></i>' +
+				'</button>' +
 			'</li>';
 
 		var Todo = A.Component.create(
@@ -18,9 +28,24 @@ AUI.add(
 
 				prototype: {
 					initializer: function(config) {
+						new A.CharCounter(
+							{
+								input: '.input-item',
+
+								counter: '.counter',
+
+								maxLength: 35
+							});
+
 						var instance = this;
 
 						var todoList = instance.one('.task-container ul');
+
+						var finishedTask = instance.one('.finished-task');
+
+						var remainingDiv = instance.one('.remaining-tasks-count');
+
+						remainingDiv.html('1');
 
 						if (todoList) {
 							todoList.delegate(
@@ -31,11 +56,52 @@ AUI.add(
 									var listItem = currentTarget.ancestor('li');
 
 									listItem.remove();
+
+									if (finishedTask) {
+
+										var finishedHtml = A.Lang.sub(
+											TPL_FINISHED,
+											{
+												taskFinished: 'Task finished!'
+											}
+										);
+
+										finishedTask.append(finishedHtml);
+
+										var taskList = A.one('task-list');
+
+										var taskListLi = taskList.all('li');
+
+										var remainingCount = taskListLi.size();
+
+										if (remainingCount) {
+											remainingDiv.html(remainingCount);
+										}
+										else if (remainingCount === 0) {
+											remainingDiv.html('0');
+										}
+									}
 								},
 								'button'
 							);
 
 							instance._todoList = todoList;
+						}
+
+						if (finishedTask) {
+							finishedTask.delegate(
+								EVENT_CLICK,
+								function(event) {
+									var currentTarget = event.currentTarget;
+
+									var finishedItem = currentTarget.ancestor('li');
+
+									finishedItem.remove();
+								},
+								'button'
+							);
+
+							instance._finishedTask = finishedTask;
 						}
 
 						var addButton = instance.byId('add');
@@ -57,6 +123,7 @@ AUI.add(
 						var instance = this;
 
 						var taskInput = instance._taskInput;
+
 						var todoList = instance._todoList;
 
 						if (taskInput && todoList) {
@@ -70,8 +137,24 @@ AUI.add(
 							todoList.append(taskHtml);
 
 							taskInput.val('');
+
+							var taskList = A.one('task-list');
+
+							var taskListLi = taskList.all('li');
+
+							var remainingCount = taskListLi.size();
+
+							var remainingDiv = instance.one('.remaining-tasks-count');
+
+							if (remainingCount) {
+								remainingDiv.html(remainingCount);
+							}
+							else if (remainingCount === 0) {
+								remainingDiv.html('0');
+							}
 						}
 					}
+
 				}
 			}
 		);
@@ -81,6 +164,6 @@ AUI.add(
 
 	'',
 	{
-		requires: ['event-key', 'node-event-delegate']
+		requires: ['aui-char-counter', 'event-key', 'node-event-delegate']
 	}
 );
